9f655ad8a9e921b2ca638ccfc6762c6e5fb3db14
puxinhe <35254811+puxinhe@users.noreply.github.com>
2018-07-08 14:50:10 +0800

    Update word2vec_basic.py

--- quiz-w10-code/word2vec_basic.py
+++ quiz-w10-code/word2vec_basic.py
@@ -54,7 +54,7 @@ def maybe_download(filename, expected_bytes):
 
 filename = maybe_download('text8.zip', 31344016)
 '''
-filename = 'quiz-w10-code/QuanSongCi.txt'
+filename = 'QuanSongCi.txt'
 # Read the data into a list of strings.
 
 def read_data(filename):
